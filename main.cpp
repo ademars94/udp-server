6a3dae5c89729c4d9421d2d4527b307758216486
Alex DeMars <alexdemars@gmail.com>
2017-10-05 17:04:22 -0700

    foo

--- main.cpp
+++ main.cpp
@@ -1,97 +1,24 @@
 #include <iostream>
 #include <sys/socket.h>
 
-
 using namespace std;
 
-// =-=-=-=-=-=-=-=-=-=
-// ------- Foo -------
-// =-=-=-=-=-=-=-=-=-=
-
-class Foo {
-  private:
-    int fooNumber;
-
-  public:
-    int getFooNumber() { return fooNumber; }
-
-    void setFooNumber(int fooNumber) {
-      this -> fooNumber = fooNumber;
-      cout << "fooNumber has been set to " << fooNumber << "." << endl;
-    }
-
-    explicit Foo(int);
-
-    Foo();
-
-    ~Foo();
-};
-
-Foo::Foo(int fooNumber) {
-  cout << "Instantiating Foo class with fooNumber " << fooNumber << "..." << endl;
-  this -> fooNumber = fooNumber;
-}
-
-Foo::Foo() {
-  this -> fooNumber = 0;
-}
-
-Foo::~Foo() {
-  cout << "Foo object deallocated." << endl;
-}
-
-// =-=-=-=-=-=-=-=-=-=
-// ------- Bar -------
-// =-=-=-=-=-=-=-=-=-=
-
-class Bar: public Foo {
-  private:
-    int barNumber;
-
-  public:
-    int getBarNumber() { return barNumber; }
-
-    void setBarNumber(int barNumber) {
-      this -> barNumber = barNumber;
-      cout << "barNumber has been set to " << barNumber << "." << endl;
-    }
-
-    explicit Bar(int);
-    Bar();
-
-    ~Bar();
-};
-
-Bar::Bar(int barNumber) {
-  cout << "Instantiating Bar class with barNumber " << barNumber << "..." << endl;
-  this -> barNumber = barNumber;
-}
-
-Bar::Bar() {
-  this -> barNumber = 0;
-}
-
-Bar::~Bar() {
-  cout << "Bar object deallocated." << endl;
-}
-
-// =-=-=-=-=-=-=-=-=-=
-// ------- Main ------
-// =-=-=-=-=-=-=-=-=-=
-
 int main() {
-  cout << "Hello, World!" << endl;
-
-  Bar bar = Bar(2);
-  cout << "Foo Number: " << bar.getFooNumber() << endl;
-  cout << "Bar Number: " << bar.getBarNumber() << endl;
-
-  bar.setBarNumber(10);
-  bar.setFooNumber(1 + 2);
-  cout << "Foo Number: " << bar.getFooNumber() << endl;
-  cout << "Bar Number: " << bar.getBarNumber() << endl;
+  cout << "Hello, World!\n" << endl;
 
 //  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  int one = 1;
+  int two = 2;
+  int three = 3;
+
+  int *oneP;
+
+  oneP = &one;
+
+  cout << "Output:" << endl;
+  cout << one << endl;
+  cout << *oneP << endl;
 
   return 0;
 }
